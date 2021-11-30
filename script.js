40d2e8bb7de01879b74d828c67ee85d7208c6991
Hwee Min <Your own email>
2021-12-01 03:55:48 +0800

    update

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ window.addEventListener("scroll",function(){
   var header = document.querySelector("header");
   header.classList.toggle("sticky",window.scrollY >0);
 })
+
+
+/* Index parallex effect */
+const parallex = document.getElementById("aboutimg");
+
+window.addEventListener("scroll",function(){
+  let offset = window.pageY0ffset;
+  parallex.style.backgroundPositionY = offset* 0.2 + "px";
+})
+
